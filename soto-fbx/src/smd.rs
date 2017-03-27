9acbe52cd75b64d0e4c7bb413d2a01fb518bfce9
Layl <LaylConway@users.noreply.github.com>
2017-03-27 17:49:56 +0200

    Add PreRotation to pivot calculation

--- soto-fbx/src/smd.rs
+++ soto-fbx/src/smd.rs
@@ -19,8 +19,9 @@ pub fn create_reference_smd(fbx: &PathBuf) -> Result<Smd, Error> {
     // Go over all FBX root nodes and turn them into SMD data
     let mut smd = Smd::new();
     process_fbx_node(
+        &fbx,
         &fbx_tree, &mut smd,
-        &Matrix4::identity(), Vector3::new(0.0, 0.0, 0.0),
+        &Matrix4::identity(),
         None
     )?;
 
@@ -71,8 +72,9 @@ pub fn create_animation_smd(ref_smd: &Smd, fbx: &PathBuf) -> Result<Smd, Error> 
 }
 
 fn process_fbx_node(
+    fbx: &SimpleFbx,
     fbx_node: &ObjectTreeNode, mut smd: &mut Smd,
-    matrix: &Matrix4<f32>, pivot: Vector3<f32>,
+    matrix: &Matrix4<f32>,
     current_bone: Option<&SmdBone>,
 ) -> Result<(), Error> {
     // Perform node type specific information
@@ -80,11 +82,11 @@ fn process_fbx_node(
         ObjectType::Geometry(ref geometry) =>
             process_geometry(smd, geometry, matrix, current_bone.unwrap()),
         ObjectType::Model(ref _model) =>
-            process_model(fbx_node, smd, matrix, pivot, current_bone)?,
+            process_model(fbx, fbx_node, smd, matrix, current_bone)?,
         _ => {
             // Just go straight to the children
             for node in &fbx_node.nodes {
-                process_fbx_node(node, smd, matrix, pivot, current_bone)?;
+                process_fbx_node(fbx, node, smd, matrix, current_bone)?;
             }
         }
     }
@@ -127,49 +129,44 @@ fn process_geometry(smd: &mut Smd, geometry: &Geometry, matrix: &Matrix4<f32>, c
 }
 
 fn process_model(
+    fbx: &SimpleFbx,
     fbx_node: &ObjectTreeNode, smd: &mut Smd,
-    matrix: &Matrix4<f32>, pivot: Vector3<f32>,
+    matrix: &Matrix4<f32>,
     current_bone: Option<&SmdBone>
 ) -> Result<(), Error> {
     task_log(format!("Adding model \"{}\" to SMD data", friendly_name(&fbx_node.object.name)));
     let properties = ModelProperties::from_generic(&fbx_node.object.properties);
 
     // Create a new transformation matrix
-    let (vertices_matrix, rot_pivot) = local_matrices(&properties);
+    let (local_matrix, _) = local_matrices(&properties);
 
-    // Create a new bone and set the transformations
+    // Create a new bone
     let new_bone = smd.new_bone(
-        &id_name(&fbx_node.object.name).unwrap(),
-        current_bone.map(|b| b.id)
-    )
+            &id_name(&fbx_node.object.name).unwrap(),
+            current_bone.map(|b| b.id)
+        )
         .ok_or_else(|| Error::Task(format!(
             "Bone \"{}\" exists multiple times in the FBX",
             &fbx_node.object.name
         )))?
         .clone(); // Clone needed to avoid a borrow since we need to mut borrow SMD later
+
+    // Set the transformations on this bone
+    let (translation, rotation) = calculate_animation_transforms_for(fbx, &fbx_node.object);
     let first_frame = SmdAnimationFrameBone {
         // This needs to be derived from the matrix to get the right location
-        translation: (
-            Matrix4::from_translation(-pivot) *
-            vertices_matrix *
-            Vector4::new(rot_pivot.x, rot_pivot.y, rot_pivot.z, 1.0)
-        ).truncate().into(),
+        translation: translation.into(),
         // This can just be directly copied over
-        rotation: [
-            Rad::from(Deg(properties.rotation[0])).0,
-            Rad::from(Deg(properties.rotation[1])).0,
-            Rad::from(Deg(properties.rotation[2])).0,
-        ],
+        rotation: rotation.into(),
     };
     smd.set_animation(0, new_bone.id, first_frame);
 
     // Make new matrices for children
-    let matrix = matrix * vertices_matrix;
-    let pivot = rot_pivot;
+    let matrix = matrix * local_matrix;
 
     // Make sure the child nodes will receive this new bone
     for node in &fbx_node.nodes {
-        process_fbx_node(node, smd, &matrix, pivot, Some(&new_bone))?;
+        process_fbx_node(fbx, node, smd, &matrix, Some(&new_bone))?;
     }
 
     Ok(())
@@ -179,47 +176,44 @@ fn process_model(
 fn calculate_animation_transforms_for(fbx: &SimpleFbx, obj: &Object) -> (Vector3<f32>, Vector3<f32>) {
     let properties = ModelProperties::from_generic(&obj.properties);
 
-    // First get the world matrix and pivot TODO: Clean this up and re-comment
-    let (_matrix, _pivot, parent_pivot) = world_matrices_of(fbx, obj);
-    let (matrix, pivot) = local_matrices(&properties);
+    // First get the matrices and pivots we need
+    let parent_pivot = get_pivot_of_parent(fbx, obj);
+    let (local_matrix, pivot) = local_matrices(&properties);
 
     // Now transform a 0,0,0 vector to get the actual pivot
     let translation = (
         Matrix4::from_translation(-parent_pivot) *
-        matrix *
+        local_matrix *
         Vector4::new(pivot.x, pivot.y, pivot.z, 1.0)
     ).truncate().into();
 
     // This can just be directly copied over
+    let pre_rotation: Vector3<f32> = properties.pre_rotation.into();
+    let rotation: Vector3<f32> = properties.rotation.into();
+    let total_rotation: Vector3<f32> = pre_rotation + rotation;
     let rotation = Vector3::new(
-        Rad::from(Deg(properties.rotation[0])).0,
-        Rad::from(Deg(properties.rotation[1])).0,
-        Rad::from(Deg(properties.rotation[2])).0
+        Rad::from(Deg(total_rotation.x)).0,
+        Rad::from(Deg(total_rotation.y)).0,
+        Rad::from(Deg(total_rotation.z)).0,
     );
 
     (translation, rotation)
 }
 
-fn world_matrices_of(fbx: &SimpleFbx, obj: &Object) -> (Matrix4<f32>, Vector3<f32>, Vector3<f32>) {
-    // Start with the local matrix
-    let (l_mat, l_piv) = local_matrices(&ModelProperties::from_generic(&obj.properties));
-
+fn get_pivot_of_parent(fbx: &SimpleFbx, obj: &Object) -> Vector3<f32> {
     // Check this obj's parents
     let parent_id = fbx.parent_of(obj.id).unwrap();
 
-    // If this is a root object, we're done, return the data we got
+    // If this is an object at root, so no parent pivot
     if parent_id == 0 {
-        return (l_mat, l_piv, Vector3::new(0.0, 0.0, 0.0))
+        return Vector3::new(0.0, 0.0, 0.0)
     }
 
-    // If this isn't a root object, we need to get that obj's data first
-    let (p_mat, p_piv, _) = world_matrices_of(fbx, &fbx.objects[&parent_id]);
-
-    // Then return our data transformed
-    (p_mat * l_mat, l_piv, p_piv)
+    // We've got a parent, get its pivot
+    local_matrices(&ModelProperties::from_generic(&obj.properties)).1
 }
 
-/// Returns (vertices_matrix, rot_pivot)
+/// Returns (vertices_matrix, pivot)
 fn local_matrices(properties: &ModelProperties) -> (Matrix4<f32>, Vector3<f32>) {
     let rotation_offset = properties.rotation_offset.into();
     let rotation_offset_mat = Matrix4::from_translation(rotation_offset);
